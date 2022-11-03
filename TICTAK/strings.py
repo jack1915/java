4d71a567aef53b1adf0cdbe6b2aa85d9a867ad30
jack1915 <109028048+jack1915@users.noreply.github.com>
2022-11-03 16:28:13 +0500

    create_Tictak

--- TICTAK/strings.py
+++ TICTAK/strings.py
@@ -10,8 +10,7 @@ ANSW_YOUR_TURN = 'Ваш ход'
 ANSW_YOU_WIN = 'Вы победили'
 ANSW_BOT_WIN = 'Победил бот'
 ANSW_DRAW = 'Ничья'
-ANSW_HELP = 'Бот для игры в крестики-нолики\nСоздатель: @pashabezk\n\n' \
-            'Используйте команду /new_game для начала новой игры'
+ANSW_HELP = 'Используйте команду /new_game для начала новой игры'
 
 
 ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
